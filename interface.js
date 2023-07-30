0a2776e953afb9b11055f5b6abe766baaf0d0c83
Veteran0411 <115995378+Veteran0411@users.noreply.github.com>
2023-07-30 14:23:42 +0530

    Update interface.js

--- interface.js
+++ interface.js
@@ -8,6 +8,14 @@ const modal_answer_section = document.getElementById("modal_answer_section");
 const varCharLabel = document.getElementById("varCharLabel");
 const holeLimit = document.getElementById("holeLimit");
 
+const holeUpperLimitAnswer = document.getElementById("holeUpperLimitAnswer");
+const holeLowerLimitAnswer = document.getElementById("holeLowerLimitAnswer");
+const shaftupperLimitAnswer = document.getElementById("shaftupperLimitAnswer");
+const shaftLowerLimitAnswer = document.getElementById("shaftLowerLimitAnswer");
+const holetoleranceAnswer = document.getElementById("holetoleranceAnswer");
+const shafttoleranceAnswer = document.getElementById("shafttoleranceAnswer");
+var flag=0;
+
 
 // input element variable below
 const varChar = document.getElementById("varChar");
@@ -24,13 +32,14 @@ cancel_button_container.addEventListener('click', () => {
     modal_answer_section.style.transform = "translate(25%,80%)";
     modal_answer_section.style.borderRadius = "30% 10% 0 0";
     modal_answer_section.style.borderRadius = "30% 10% 0 0";
-    modal_answer_section.style.color = "#6528F7";
+    // modal_answer_section.style.color = "#6528F7";
+    holeLimit.style.color = "#6528F7";
     cancel_button.style.color = "#6528F7";
     varCharLabel.style.transform = "translateX(-60px";
     varCharLabel.style.fontSize = "1.3rem";
     varChar.value = " ";
-    errorText.innerHTML=" ";
-    
+    errorText.innerHTML = " ";
+
 })
 
 function upAndValidate() {
@@ -44,9 +53,11 @@ function upAndValidate() {
         modal_answer_section.style.transform = "translate(0%,0%)";
         modal_answer_section.style.borderRadius = "0%";
         cancel_button_container.style.color = "white";
+        
         cancel_button_container.style.backgroundColor = "white";
         cancel_button.style.color = "#6528F7";
-        holeLimit.style.color="white";
+        
+        holeLimit.style.color = "white";
         try {
             Main(str);
         }
@@ -73,39 +84,39 @@ function fundamental(val, D) {
     const b = val.toLowerCase();
     const dic = {
         "a": (D <= 120) ? -(265 + 1.3 * D) : -3.5 * D,
-        "b": -12 * Math.pow(D, 0.56),
-        "c": -6 * Math.pow(D, 0.44),
-        "d": -16 * Math.pow(D, 0.44),
-        "e": -8 * Math.pow(D, 0.34),
-        "f": -2.5 * Math.pow(D, 0.34),
-        "g": -2.5 * Math.pow(D, 0.34),
+        "b": (D <= 160) ? -(140 + 0.85 * D) : -1.8 * D,
+        "c": (D <= 40) ? -(52*Math.pow(D,0.2)) : -(9.5+0.8*D),
+        "d": -16 * Math.pow(D, 0.41),
+        "e": -11 * Math.pow(D, 0.41),
+        "f": -5.5 * Math.pow(D, 0.41),
+        "g": -5.5 * Math.pow(D, 0.34),
         "h": 0,
         "i": -0.6 * Math.pow(D, 0.5),
         "j": -0.4 * Math.pow(D, 0.4),
         "k": -0.2 * Math.pow(D, 0.3),
         "l": -0.1 * Math.pow(D, 0.2),
-        "m": -0.08 * D,
-        "n": -0.04 * D,
+        "m": (16*((0.45 * Math.cbrt(D)) + (0.001 * D))-10*((0.45 * Math.cbrt(D)) + (0.001 * D))),
+        "n": 5*Math.pow(D,0.24),
         "o": 0,
         "p": 0,
         "q": 0,
         "r": 0,
         "s": 0,
-        "t": 0,
+        "t": 16*((0.45 * Math.cbrt(D)) + (0.001 * D)) + 0.63*D,
         "u": 0,
         "v": 0,
         "w": 0,
-        "x": 0,
+        "x": 177 + 1.6*D,
         "y": 0,
         "z": 0,
-        "cd": (D <= 120) ? -(12 + 0.8 * D) : -2 * D,
+        "za": (D <= 120) ? -(12 + 0.8 * D) : -2 * D,
         "ef": -6 * Math.pow(D, 0.44),
         "fg": -2.5 * Math.pow(D, 0.34),
         "js": -0.8 * Math.pow(D, 0.5),
     };
     // console.log(dic[b])
-    // console.log("Checking",parseFloat(dic[b]).toFixed(4));
-    return parseFloat(dic[b]).toFixed(4);
+    // console.log("Checking",parseFloat(dic[b]) );
+    return parseFloat(dic[b]) ;
 }
 
 // function to get geometric mean for D
@@ -138,9 +149,11 @@ function getDiameterValue(n) {
     }
 }
 
+
 // function to calculate IT_Tolerance values
 function getItToleranceValues(it_number) {
     const ItRange = new Map([
+        ["IT5", 7],
         ["IT6", 10],
         ["IT7", 16],
         ["IT8", 25],
@@ -164,35 +177,47 @@ function getItToleranceValues(it_number) {
 
 function Main(str) {
     const extracted_num = str.match(/(\d+)/g);
-    // console.log(extracted_num)
+    console.log(extracted_num)
     const extracted_char = str.match(/(\D+)/g);
-    // console.log(extracted_char)
-    // console.log(extracted_char)
-    const zeroLine = parseFloat(extracted_num[0]).toFixed(4); // or basic size ,i removed parsefloat which was out
+    console.log(extracted_char)
+    console.log(extracted_char)
+    const zeroLine = parseFloat(extracted_num[0]) ; // or basic size ,i removed parsefloat which was out
     const D = getDiameterValue(extracted_num[0]);
-    // console.log(D, "D", typeof (D));
-    const i = ((0.45 * Math.cbrt(D)) + (0.001 * D)).toFixed(4);
-    let hole_var = parseFloat(fundamental(extracted_char[0], D));
-    // console.log("fundamental", hole_var)//values are returned num,ber
-    // console.log(hole_var, "checking", typeof (hole_var));
-    let shaft_var = parseFloat(fundamental(extracted_char[1], D));
+    const i = ((0.45 * Math.cbrt(D)) + (0.001 * D)) ;
+    let hole_var = parseFloat(fundamental(extracted_char[0], D))/1000;
+    let shaft_var = parseFloat(fundamental(extracted_char[1], D))/1000;
     const i_holeToleranceForm = "IT" + extracted_num[1]; // convert to string of error
-    const i_holeToleranceValue = parseFloat(getItToleranceValues(i_holeToleranceForm)).toFixed(4);
-    const holeTolerance = parseFloat((parseFloat(i) * parseFloat(i_holeToleranceValue)).toFixed(4));
+    const i_holeToleranceValue = parseFloat(getItToleranceValues(i_holeToleranceForm)) ;
+    const holeTolerance = parseFloat(((parseFloat(i) * parseFloat(i_holeToleranceValue))/1000) );
     const i_shaftToleranceForm = "IT" + extracted_num[2];
     const i_shaftToleranceValue = getItToleranceValues(i_shaftToleranceForm);
-    const shaftTolerance = parseFloat((parseFloat(i) * parseFloat(i_shaftToleranceValue)).toFixed(4));
+    const shaftTolerance = parseFloat(((parseFloat(i) * parseFloat(i_shaftToleranceValue))/1000) );
 
     // calculation of limits
-    // console.log(typeof (zeroLine), "zeroLine", zeroLine)
-    // console.log(typeof (holeTolerance), "holeTolerance", holeTolerance)
-    const holeMaxLimit = parseFloat(zeroLine+ holeTolerance);  // as 1 mm is 1000 micro meter , the result of this multiplication will be micrometer
-    // console.log("holeMaxLimit", typeof (holeMaxLimit))
-    const holeMinLimit = (zeroLine + hole_var); //  + hole_var was removed zero line and fundamental deviation are in mm so converted in micro meter
+    const holeMaxLimit = parseFloat(zeroLine) + holeTolerance + hole_var;  // as 1 mm is 1000 micro meter , the result of this multiplication will be micrometer
+    const holeMinLimit =parseFloat(zeroLine) + hole_var; //  + hole_var was removed zero line and fundamental deviation are in mm so converted in micro meter
 
-    // i need to confirm this
-    const shaftMinSize = parseFloat((zeroLine - (shaftTolerance - shaft_var) * 1e-3, 3).toFixed(4));
-    const shaftMaxSize = parseFloat((zeroLine + shaft_var * 1e-3, 3).toFixed(4));
+
+    // calculation for shaft
+    var shaftMinSize;
+    var shaftMaxSize
+    if("a"<=extracted_char[1] && extracted_char[1] <="h"){
+        shaftMinSize  = (parseFloat(zeroLine) - shaftTolerance +shaft_var) ;
+        console.log("min shaft",shaftMinSize);
+        console.log(" shaftTolerance",shaftTolerance);
+        console.log(" shaft_var",shaft_var);
+        shaftMaxSize= (parseFloat(zeroLine) + shaft_var) ;
+        console.log("max shaft",shaftMaxSize);
+    }
+    else{
+        shaftMaxSize = (parseFloat(zeroLine) + shaftTolerance +shaft_var) ;
+        console.log("min shaft",shaftMinSize);
+        console.log(" shaftTolerance",shaftTolerance);
+        console.log(" shaft_var",shaft_var);
+         shaftMinSize = (parseFloat(zeroLine) + shaft_var) ;
+        console.log("max shaft",shaftMaxSize);
+    }
+
 
     // calculation clearance
     const maxClearance = holeMaxLimit - shaftMinSize;
@@ -206,17 +231,22 @@ function Main(str) {
         fitType = "Interference Fit";
     } else {
         fitType = "Transition Fit";
-
-
     }
-    
+
     console.log(`Limits for the hole: ${holeMinLimit} mm - ${holeMaxLimit} mm`);
 
     console.log(`Limits for the shaft: ${shaftMinSize} mm - ${shaftMaxSize} mm`);
 
     console.log(`Fit type: ${fitType}`);
 
-    console.log(`Tolerance for hole  is ${holeTolerance} μm`);
+    console.log(`Tolerance for hole  is ${holeTolerance*1000} μm`);
 
-    console.log(`Tolerance for shaft  is ${shaftTolerance} μm`);
+    console.log(`Tolerance for shaft  is ${shaftTolerance*1000} μm`);
+
+    holeUpperLimitAnswer.innerHTML=holeMaxLimit.toFixed(3);
+    holeLowerLimitAnswer.innerHTML=holeMinLimit.toFixed(3);
+    shaftupperLimitAnswer.innerHTML=shaftMaxSize.toFixed(3);
+    shaftLowerLimitAnswer.innerHTML=shaftMinSize.toFixed(3);
+    holetoleranceAnswer.innerHTML=Math.round(holeTolerance*1000);
+    shafttoleranceAnswer.innerHTML=Math.round(shaftTolerance*1000);
 }
